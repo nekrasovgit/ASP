1e5dd60bb04bb88c9b1b1f986e3f241cb411b89d
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-07-20 21:20:39 +0300

    Create GoodNewsContext.cs

---
+++ DNekrasovDB/DNekrasovDB/Models/DB/GoodNewsContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DNekrasovDB.Models.DB
+{
+    public class GoodNewsContext : DbContext
+    {
+        public DbSet<Comments> Comments { get; set; }
+
+        public DbSet<Magazine> Magazines { get; set; }
+
+        public DbSet<News> News { get; set; }
+
+        public DbSet<Roles> Roles { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<UserRoles> UserRoles { get; set; }
+
+        public GoodNewsContext(DbContextOptions<GoodNewsContext> options) : base(options)
+        {
+
+        }
+    }
+}
