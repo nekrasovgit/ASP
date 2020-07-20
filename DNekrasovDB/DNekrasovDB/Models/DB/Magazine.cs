f38934068f2415a0ff78849cc522e692eacf6613
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-07-20 21:20:42 +0300

    Create Magazine.cs

---
+++ DNekrasovDB/DNekrasovDB/Models/DB/Magazine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DNekrasovDB.Models.DB
+{
+    public class Magazine
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+
+    }
+}
