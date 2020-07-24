bb78b73d93d22982e671d407b5a72389c7f128f7
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-07-24 12:41:52 +0300

    Create News.cs

---
+++ DNekrasovDB/DNekrasovDB/Models/DB/News.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DNekrasovDB.Models.DB
+{
+    public class News
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Body { get; set; }
+
+        public IEnumerable<Comments> Comments { get; set; }
+
+        public Guid MagazineId { get; set; }
+
+        public Magazine Magazine { get; set; }
+    }
+}
