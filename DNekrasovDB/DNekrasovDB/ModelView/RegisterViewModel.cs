eeba9ff146f0a2356b2dd1706380576f769d9416
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-08-20 14:49:51 +0300

    Create RegisterViewModel.cs

---
+++ DNekrasovDB/DNekrasovDB/ModelView/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DNekrasovDB.ModelView
+{
+    public class RegisterViewModel
+    {
+        
+
+        [Required(ErrorMessage ="Email is required faild")]
+        [DataType(DataType.EmailAddress)]
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required faild")]
+        [DataType(DataType.Password)]
+
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Password don't match")]
+        [DataType(DataType.Password)]
+
+        public string PasswordConfiguration { get; set; }
+    }
+}
