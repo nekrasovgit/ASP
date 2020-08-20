70db7011e6ba080ab3117e07b7ed163c35b1ae1a
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-08-20 14:48:55 +0300

    Create RssRdeader.cs

---
+++ DNekrasovDB/DNekrasovDB/Data/RssReader/RssRdeader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using Microsoft.Extensions.Logging;
+
+namespace DNekrasovDB.Data.RssReader
+{
+    public class RssReader : IRssReader
+    {
+        private readonly ILogger _logger;
+
+        public RssReader(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public IEnumerable<SyndicationItem> GetNewsFromFeed(string feedurl)
+        {
+            try
+            {
+
+
+                using (var reader = XmlReader.Create(feedurl))
+                {
+                    var feed = SyndicationFeed.Load(reader);
+
+                    foreach (var item in feed.Items)
+                    {
+                        item.SourceFeed = feed;
+                    }
+
+                    var mynews = feed.Items.ToArray();
+                    return mynews;
+                }
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                throw;
+            }
+        }
+    }
+
+
+}
