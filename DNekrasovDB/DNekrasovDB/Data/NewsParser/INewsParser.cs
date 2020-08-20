54766448839f7edca6dde6f5cdd2ee1a743eaf6e
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-08-20 14:48:18 +0300

    Create INewsParser.cs

---
+++ DNekrasovDB/DNekrasovDB/Data/NewsParser/INewsParser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DNekrasovDB.Data.NewsParser
+{
+    public interface INewsParser
+    { 
+        void Parse(string rssurl);
+    }
+}
