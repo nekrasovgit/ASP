34583190fa953fc62bc497d0cb41a10053a47f8d
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-08-20 14:48:15 +0300

    Create TestController.cs

---
+++ DNekrasovDB/DNekrasovDB/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DNekrasovDB.Data.Repository;
+using DNekrasovDB.Data.UnitOfWork;
+using DNekrasovDB.Models.DB;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DNekrasovDB.Controllers
+{
+    public class TestController : Controller
+    {
+        
+        private readonly IUnitOfWork _unitofwork;
+
+        public TestController(IUnitOfWork unitofwork)
+        {
+            _unitofwork = unitofwork;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            
+            await _unitofwork.MagazineRepository
+                .AddAsync(new Magazine() { Id = Guid.NewGuid(), Name = "Onliner", RssUrl = @"onliner.by"});
+            await _unitofwork.SaveChangesAsync();
+
+            return View();
+        }
+    }
+}
