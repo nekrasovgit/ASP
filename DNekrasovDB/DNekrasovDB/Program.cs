720e08804bdd6bbcd587780a40843362f998e418
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-08-20 14:49:57 +0300

    Update Program.cs

--- DNekrasovDB/DNekrasovDB/Program.cs
+++ DNekrasovDB/DNekrasovDB/Program.cs
@@ -1,11 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace DNekrasovDB
 {
@@ -13,11 +9,18 @@ namespace DNekrasovDB
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.File(@"D:\C#\log\log-.log", rollingInterval: RollingInterval.Minute)
+            .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
