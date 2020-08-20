71f2d56d081c1b186a3f5b5c911133e9586deb2a
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-08-20 14:48:58 +0300

    Create Storage.cs

---
+++ DNekrasovDB/DNekrasovDB/Data/Storage/Storage.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace DNekrasovDB.Data.Storage
+{
+    public class Storage
+    {
+
+        
+        
+        public static string[] rssFeeds = {
+            @"https://news.tut.by/rss/all.rss",
+        @"http://s13.ru/rss",
+        @"https://tech.onliner.by/feed",
+        };
+
+
+
+
+        
+    }
+}
