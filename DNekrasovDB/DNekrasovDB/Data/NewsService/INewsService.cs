2bdc00b421be77b8f45994dfcb1862b63921ed2a
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-08-20 14:48:23 +0300

    Create INewsService.cs

---
+++ DNekrasovDB/DNekrasovDB/Data/NewsService/INewsService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ServiceModel.Syndication;
+
+namespace DNekrasovDB.Data.NewsService
+{
+    public interface INewsService
+    {
+        public void GetDataFromRssInsertIntoDB();
+
+        
+    }
+}
