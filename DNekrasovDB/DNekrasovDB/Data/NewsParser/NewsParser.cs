0ded2c4b69dfd0e9de135bfe250c8c5a0568398a
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-08-20 14:48:20 +0300

    Create NewsParser.cs

---
+++ DNekrasovDB/DNekrasovDB/Data/NewsParser/NewsParser.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace DNekrasovDB.Data.NewsParser
+{
+    class NewsParser : INewsParser
+    {
+        public void Parse(string rssurl)
+        {
+            var url = "http://html-agility-pack.net/";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+
+            var docNode = doc.DocumentNode;
+
+            var title = doc.DocumentNode.Descendants("h1").
+               FirstOrDefault(x => x?.Attributes["itemprop"]?.Value == "headline");
+        
+        
+            var body = doc.DocumentNode.Descendants("div").
+               Where(x => x?.Attributes["id"]?.Value == "article_body").ToList();
+
+        }
+        
+    }
+}
