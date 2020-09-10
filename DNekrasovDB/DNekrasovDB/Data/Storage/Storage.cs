09c4dfdb7ce2c8dc7006300822dd9150d4ebc0a6
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-09-10 19:06:28 +0300

    Update Storage.cs

--- DNekrasovDB/DNekrasovDB/Data/Storage/Storage.cs
+++ DNekrasovDB/DNekrasovDB/Data/Storage/Storage.cs
@@ -1,21 +1,35 @@
 ﻿
 
+using DNekrasovDB.Models.DB;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+
 namespace DNekrasovDB.Data.Storage
 {
     public class Storage
     {
+        private readonly GoodNewsContext _goodNewsContext;
 
-        
-        
+        public Storage(GoodNewsContext goodNewsContext)
+        {
+            _goodNewsContext = goodNewsContext;
+
+        }
+
+
         public static string[] rssFeeds = {
             @"https://news.tut.by/rss/all.rss",
         @"http://s13.ru/rss",
         @"https://tech.onliner.by/feed",
         };
 
+        //public static List<News> News { get; set; }
 
 
 
-        
+
     }
 }
