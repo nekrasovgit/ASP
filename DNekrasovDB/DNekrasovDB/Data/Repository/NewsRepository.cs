9eba435d8da80e582c33a8011d0f7defd51e89e8
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-09-10 19:06:20 +0300

    Update NewsRepository.cs

--- DNekrasovDB/DNekrasovDB/Data/Repository/NewsRepository.cs
+++ DNekrasovDB/DNekrasovDB/Data/Repository/NewsRepository.cs
@@ -1,4 +1,11 @@
 ﻿using DNekrasovDB.Models.DB;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DNekrasovDB.Data.Repository
 {
@@ -6,6 +13,18 @@ namespace DNekrasovDB.Data.Repository
     {
         public NewsRepository(GoodNewsContext goodNewsContext) : base(goodNewsContext)
         {
+        }
+
+
+
+        public override async Task<IEnumerable<News>> GetAllAsync()
+        {
+            return await _dbset.ToListAsync();
+        }
+
+        public override async Task AddRangeasync(IEnumerable<News> obj)
+        {
+            await _dbset.AddRangeAsync(obj);
         }
     }
 }
