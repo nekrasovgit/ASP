73e685b75ba2fb47b84729b30b0389bcf27ff0f3
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-11-30 17:21:38 +0300

    Create RoomContext.cs

---
+++ Hotel/ClassLibrary1/RoomContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class RoomContext : DbContext
+    {
+        public DbSet<Room> Rooms { get; set; }
+
+        public RoomContext(DbContextOptions<RoomContext> options) : base(options)
+        {
+
+        }
+    }
+}
