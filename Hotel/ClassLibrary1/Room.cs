addd233d4b6008fb33ae1ea462ef05ea224eadfe
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-11-30 17:21:43 +0300

    Create Room.cs

---
+++ Hotel/ClassLibrary1/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Model
+
+{
+    public class Room
+    {
+        public Guid Id { get; set; }
+
+        public int Number { get; set; }
+
+        public DateTime CheckIn { get; set; }
+
+        public DateTime CheckOut { get; set; }
+
+        public int NumberOfPeople { get; set; }
+
+        public decimal PriceForOneNight { get; set; }
+    }
+}
