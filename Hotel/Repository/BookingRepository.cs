8bc48b3c08b2613484e2887cab370284377be469
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-11-30 17:21:17 +0300

    Create BookingRepository.cs

---
+++ Hotel/Repository/BookingRepository.cs
@@ -0,0 +1,28 @@
+﻿using Model;
+using System;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class BookingRepository : IBookingRepository
+    {
+        private readonly RoomContext _roomContext;
+
+        public BookingRepository(RoomContext roomContext)
+        {
+            _roomContext = roomContext;
+        }
+
+        public async Task AddRoomAsync(Room room)
+        {
+            _roomContext.Rooms.Add(room);
+            await SaveChangeAsync();
+        }
+
+        public async Task SaveChangeAsync()
+        {
+            await _roomContext.SaveChangesAsync();
+
+        }
+    }
+}
