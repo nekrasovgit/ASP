2e71dc3ba2ed80716760fc4a28ed889999974c41
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-11-30 17:21:00 +0300

    Create BookingService.cs

---
+++ Hotel/Service/BookingService.cs
@@ -0,0 +1,18 @@
+﻿using Repository;
+using System;
+
+namespace Service
+{
+    public class BookingService : IBookingService
+    {
+        private readonly IBookingRepository _bookingRepository;
+
+        public BookingService(IBookingRepository bookingRepository)
+        {
+            _bookingRepository = bookingRepository;
+        }
+
+
+
+    }
+}
