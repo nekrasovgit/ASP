539e2ae5793d2fa610b20d873c3ae8a8b398e76e
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2020-12-27 19:40:49 +0300

    Create Drawer.js

---
+++ src/Components/Drawer.js
@@ -0,0 +1,30 @@
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import { makeStyles } from '@material-ui/core/styles';
+import { mainListItems, secondaryListItems } from './ListItems';
+
+const useStyles = makeStyles((theme) => ({
+    toolbarIcon: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+    },
+}));
+function Draw() {
+    const classes = useStyles();
+
+    return (
+        <Drawer variant="permanent">
+            <div className={classes.toolbarIcon}></div>
+            <Divider />
+            <List>{mainListItems}</List>
+            <Divider />
+            <List>{secondaryListItems}</List>
+        </Drawer>
+    );
+}
+
+export default Draw;
