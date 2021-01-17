5239c7b74d20565659f9091f3861627df93f6856
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2021-01-18 01:59:53 +0300

    Create Job.cs

---
+++ BookingService/Quartz/Job/Job.cs
@@ -0,0 +1,31 @@
+﻿using BookingService.BookingService;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingService.Quartz.Job
+{
+    [DisallowConcurrentExecution]
+    public class Job : IJob
+    {
+        private readonly IServiceScopeFactory serviceScopeFactory;
+
+        public Job(IServiceScopeFactory serviceScopeFactory)
+        {
+            this.serviceScopeFactory = serviceScopeFactory;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            using (var scope = serviceScopeFactory.CreateScope())
+            {
+                var sheckReservation = scope.ServiceProvider.GetService<IBookingService>();
+
+                await sheckReservation.CheckReservation();
+            };
+        }
+    }
+}
