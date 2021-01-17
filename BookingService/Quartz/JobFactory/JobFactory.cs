b19cbfc0b7e28666c7593076f3f48ea1b2610982
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2021-01-18 02:00:01 +0300

    Create JobFactory.cs

---
+++ BookingService/Quartz/JobFactory/JobFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using Quartz.Spi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingService.Quartz.JobFactory
+{
+    public class JobFactory : IJobFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public JobFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public IJob NewJob(TriggerFiredBundle bundle, IScheduler scheduler)
+        {
+            return _serviceProvider.GetRequiredService<JobRunner>();
+
+        }
+
+        public void ReturnJob(IJob job)
+        {
+            //Do something if need
+        }
+    }
+}
