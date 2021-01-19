8ec33db0f658b6ddb6bce06da44fcc4bbb2a3d82
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2021-01-20 01:01:39 +0300

    Create OrderService.cs

---
+++ train/OrderService/OrderService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Train.AccountService;
+using Train.Model;
+
+namespace Train.OrderService
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IAccountService _accountService;
+        
+        public OrderService(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        public string PayRoom(OrderModel model)
+        {
+
+            var newOrder = new Order()
+            {
+                Id = Guid.NewGuid(),
+                UserId = model.UserId,
+                RoomId = model.RoomId,
+                ReservationId = model.ReservationId,
+                ReservStartDate = model.ReservStartDate,
+                ReservFinishDate = model.ReservFinishDate,
+                PriceForNight = model.PriceForNight,
+                DateOfPayment = model.DateOfPayment,
+                AmountPaid = model.AmountPaid,
+                
+            };
+
+            var response = _accountService.HandlePayment(newOrder.AmountPaid);
+
+            return response;
+        }
+    }
+}
