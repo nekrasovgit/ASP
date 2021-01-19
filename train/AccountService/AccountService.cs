63d037728ca1266a5ba69f4a076e5f881215dde2
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2021-01-20 01:01:07 +0300

    Create AccountService.cs

---
+++ train/AccountService/AccountService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using Train.Model;
+
+namespace Train.AccountService
+{
+    public class AccountService
+    {
+        public string HandlePayment(int amountPaid)
+        {
+            try
+            {
+                var newAccount = new UserAccount()
+                {
+                    Id = Guid.NewGuid(),
+                    UserId = Guid.NewGuid(),
+                    Summ = 500,
+                };
+
+                var cashStore = new CashStore()
+                {
+                    Store = 0
+                };
+
+                if (newAccount.Summ > amountPaid)
+                {
+                    cashStore.Store = newAccount.Summ - amountPaid;
+                }
+                else
+                {
+                    throw new Exception("I'm sorry, but it appears your account has insufficient funds.");
+                }
+
+                return "payment was successfully";
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+    }
+}
