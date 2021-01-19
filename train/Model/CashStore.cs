0b09bc3b4b51712ec2e4fe5424b9d69d03c3964b
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2021-01-20 01:01:16 +0300

    Create CashStore.cs

---
+++ train/Model/CashStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Train.Model
+{
+    public class CashStore
+    {
+        public int Store { get; set; }
+    }
+}
