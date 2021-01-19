c6c5146f26c5616ab881c285a975482a297a1e11
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2021-01-20 01:01:35 +0300

    Create IOrderService.cs

---
+++ train/OrderService/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Train.Model;
+
+namespace Train.OrderService
+{
+    public interface IOrderService
+    {
+        string PayRoom(OrderModel model);
+    }
+}
