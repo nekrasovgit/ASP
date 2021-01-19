458f60e6f122c381c040d3b05423b656b04b221a
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2021-01-20 01:01:11 +0300

    Create IAccountService.cs

---
+++ train/AccountService/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Train.AccountService
+{
+    public interface IAccountService
+    {
+        string HandlePayment(int amountPaid);
+    }
+}
