3474c7b87e76a31bda730b6c9a7d67f74eaee206
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2021-01-20 01:01:29 +0300

    Create UserAccount.cs

---
+++ train/Model/UserAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Train.Model
+{
+    public class UserAccount
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int Summ { get; set; }
+    }
+}
