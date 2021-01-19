90d9208704089924209308f733582d49a158c333
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2021-01-20 01:01:20 +0300

    Create Order.cs

---
+++ train/Model/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Train.Model
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid RoomId { get; set; }
+        public Guid ReservationId { get; set; }
+        public DateTime ReservStartDate { get; set; }
+        public DateTime ReservFinishDate { get; set; }
+        public int PriceForNight { get; set; }
+        public DateTime DateOfPayment { get; set; }
+        public int AmountPaid { get; set; }
+        
+    }
+}
