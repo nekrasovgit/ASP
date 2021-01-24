a159161a933b397ecb9a909924ebcc7e30f8a5e0
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2021-01-24 23:56:25 +0300

    Create IBookingService.cs

---
+++ train/BookingService/BookingService/IBookingService.cs
@@ -0,0 +1,19 @@
+﻿using BookingService.Subscriber;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingService.BookingService
+{
+    public interface IBookingService
+    {
+        Task<string> CreateReservation(BookingRequestModel model);
+        Task<string> CancelReservation(Guid id);
+        IEnumerable<ReservationDTO> GetReservation();
+        Task CheckReservation(JobMessage message);
+        Task<string> VerifyReservationId(VerificationReservationId verification);
+        Task ChangeStatus(Payment payment);
+        Task DeleteReservation(DeleteMessage message);
+    }
+}
