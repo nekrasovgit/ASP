b5e16a30ddace97cbb25fbdbd638f5aab76b9723
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2021-01-24 23:57:17 +0300

    Create ISubscriber.cs

---
+++ train/BookingService/Subscriber/ISubscriber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingService.Subscriber
+{
+    public interface ISubscriber
+    {
+        void SubscribeJobMessage();
+        Task SubscribeVerificationReservationId();
+        void SubscribePayment();
+        void SubscribeDeleteMessage();
+
+    }
+}
