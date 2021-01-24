20583d9eb38d7890cdd229cec30739655c8e6995
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2021-01-24 23:56:47 +0300

    Create IPublisher.cs

---
+++ train/BookingService/Publisher/IPublisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheduler.Publisher
+{
+    public interface IPublisher
+    {
+        Task PublishJobMessage(JobMessage message);
+        Task PublishDeleteMessage(DeleteMessage message);
+    }
+}
