bf398e8388969e8ab5f3a43636edaf73fd1867d1
nekrasovgit <62699899+nekrasovgit@users.noreply.github.com>
2021-01-24 23:56:52 +0300

    Create Publisher.cs

---
+++ train/BookingService/Publisher/Publisher.cs
@@ -0,0 +1,46 @@
+﻿using EasyNetQ;
+using EasyNetQ.Topology;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheduler.Publisher
+{
+    public class Publisher : IPublisher
+    {
+        private readonly IAdvancedBus _bus;
+
+        public Publisher(IAdvancedBus bus)
+        {
+            _bus = bus;
+        }
+
+        public async Task PublishJobMessage(JobMessage message)
+        {
+            var queueExhange = nameof(JobMessage);
+            var queue2 = _bus.QueueDeclare(queueExhange);
+            var exchange = _bus.ExchangeDeclare(queueExhange, ExchangeType.Topic);
+            _bus.Bind(exchange, queue2, "A.*");
+
+            var topic = $"ProjectId.CabinId";
+            var yourMessage = new Message<string>(JsonConvert.SerializeObject(message));
+            await _bus.PublishAsync(exchange, "A.*", true, yourMessage);
+
+        }
+
+        public async Task PublishDeleteMessage(DeleteMessage message)
+        {
+            var queueExhange = nameof(DeleteMessage);
+            var queue2 = _bus.QueueDeclare(queueExhange);
+            var exchange = _bus.ExchangeDeclare(queueExhange, ExchangeType.Topic);
+            _bus.Bind(exchange, queue2, "A.*");
+
+            var topic = $"ProjectId.CabinId";
+            var yourMessage = new Message<string>(JsonConvert.SerializeObject(message));
+            await _bus.PublishAsync(exchange, "A.*", true, yourMessage);
+
+        }
+    }
+}
